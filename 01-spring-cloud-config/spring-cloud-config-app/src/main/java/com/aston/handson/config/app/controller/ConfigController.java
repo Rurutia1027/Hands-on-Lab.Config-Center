6b8c67423ba9834d0e84c78cf6062f803b586a20
Rurutia1027 <rurutia1027@gmail.com>
2025-09-17 18:43:28 +0800

    update codes for the client(app) side

---
+++ 01-spring-cloud-config/spring-cloud-config-app/src/main/java/com/aston/handson/config/app/controller/ConfigController.java
@@ -0,0 +1,17 @@
+package com.aston.handson.config.app.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${custom.config.example:default-value}")
+    private String exampleConfig;
+
+    @GetMapping("/config/example")
+    public String getExampleConfig() {
+        return exampleConfig;
+    }
+}
---
+++ 01-spring-cloud-config/spring-cloud-config-app/src/main/java/com/aston/handson/config/app/handler/ConfigChangeLogger.java
@@ -0,0 +1,18 @@
+package com.aston.handson.config.app.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Slf4j
+@Component
+public class ConfigChangeLogger implements ApplicationListener<EnvironmentChangeEvent> {
+    @Override
+    public void onApplicationEvent(EnvironmentChangeEvent event) {
+        Set<String> changedKeys = event.getKeys();
+        log.info("Detected configuration changes: {}", changedKeys);
+    }
+}
